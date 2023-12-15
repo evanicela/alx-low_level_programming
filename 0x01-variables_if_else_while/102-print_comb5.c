10c801624a0b0472311a31a3572942a109591786
evanicela <niyonizigiye.celestin@gmail.com>
2023-12-15 12:58:59 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
  *
- * Description: Prints all possible combinations of two two-digit numbers,
- * ranging from 0 to 99. The numbers are separated by a space and printed
- * with two digits, and combinations are separated by ",", followed by a space.
- * The combinations are printed in ascending order. Duplicates are considered
- * the same combination. Uses only the putchar function eight times.
- *
- * Return: (0) - Always 0 (success)
+ * Return: Always 0.
  */
 int main(void)
 {
-for (int num1 = 0; num1 <= 99; num1++)
-{
-for (int num2 = num1; num2 <= 99; num2++)
-{
-putchar((num1 / 10) + '0');
-putchar((num1 % 10) + '0');
-putchar(' ');
-putchar((num2 / 10) + '0');
-putchar((num2 % 10) + '0');
-/* Check if it's not the last combination, then print ", " */
-if (!(num1 == 99 && num2 == 99))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
