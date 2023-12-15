c73b9ef534212d00b2c0f58cf97be4f05148f4cc
evanicela <niyonizigiye.celestin@gmail.com>
2023-12-15 12:42:40 -0800

    program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point of the program
-*
-* Description: Prints all possible different combinations of three digits,
-* separated by ",". The three digits must be different, and permutations
-* are considered the same combination. Prints only the smallest combination.
-* Uses only the putchar function six times.
-*
-* Return: (0) - Always 0 (success)
-*/
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-for (int digit1 = 0; digit1 <= 7; digit1++)
+int digit1, digit2, digit3;
+for (digit1 = 0; digit1 < 8; digit1++)
 {
-for (int digit2 = digit1 + 1; digit2 <= 8; digit2++)
+for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 {
-for (int digit3 = digit2 + 1; digit3 <= 9; digit3++)
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 {
-putchar(digit1 + '0');
-putchar(digit2 + '0');
-putchar(digit3 + '0');
-/* Check if it's not the last combination, then print "," */
-if (!(digit1 == 7 && digit2 == 8 && digit3 == 9))
-{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+putchar((digit3 % 10) + '0');
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
