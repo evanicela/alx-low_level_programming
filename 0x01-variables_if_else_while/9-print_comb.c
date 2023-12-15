ef4505cc669435af14cb66ef0cde841212a2d718
evanicela <niyonizigiye.celestin@gmail.com>
2023-12-15 11:52:14 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,28 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point of the program
  *
  * Description: Prints all possible combinations of single-digit numbers,
- * separated by ",", in ascending order. Uses only the putchar function
+ * separated by ", ", in ascending order. Uses only the putchar function
  * four times.
  *
  * Return: (0) - Always 0 (success)
  */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 <= 9; digit1++)
+for (int digit1 = 0; digit1 <= 9; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+for (int digit2 = digit1 + 1; digit2 <= 9; digit2++)
 {
 putchar(digit1 + '0');
 putchar(digit2 + '0');
-/* Check if it's the last combination, don't print "," */
-if (digit1 != 8 || digit2 != 9)
+/* Check if it's the last combination, don't print ", " */
+if (!(digit1 == 8 && digit2 == 9))
 {
 putchar(',');
-putchar(' ')
+putchar(' ');
 }}
 }
 putchar('\n');
